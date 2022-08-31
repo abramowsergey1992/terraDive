a2698feb6884d3651891d6c616c663429c7c9616
Сергей Абрамов <abramowsergey1992@gmail.com>
2022-08-31 15:38:37 +0500

    page story

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,53 +1,4 @@
 $(function () {
-	if ($(".blog-page").length) {
-		const BlogTypeSlider = new Swiper(".blog-type-slider", {
-			allowTouchMove: "false",
-			loop: false,
-			autoHeight: true,
-		});
-		$(".blog-page__head-filter").click(function () {
-			$(".blog-page__head-filter").removeClass("_active");
-			$(this).addClass("_active");
-			BlogTypeSlider.slideTo($(this).data("slideindex"));
-		});
-		$(".blog-page__filter").click(function () {
-			let filter = $(this).data("filter");
-			$(".blog-page__filter").removeClass("_active");
-			$(this).addClass("_active");
-			$(".aticle-preview").removeClass("_hidden");
-			$(".aticle-month-group").removeClass("_hidden");
-			$(".news-preview").removeClass("_hidden");
-			if (filter != "All") {
-				$(".aticle-month-group").each(function () {
-					let vLenght = 0;
-					$(this)
-						.find(".aticle-preview")
-						.each(function () {
-							if ($(this).data("filter") == filter) {
-								vLenght++;
-							} else {
-								$(this).addClass("_hidden");
-							}
-						});
-					if (vLenght == 0) {
-						$(this).addClass("_hidden");
-					} else {
-						$(this).removeClass("_hidden");
-					}
-				});
-				$(".news-preview").each(function () {
-					if ($(this).data("filter") != filter) {
-						$(this).addClass("_hidden");
-					}
-				});
-			}
-		});
-		$(".blog-page__filter._active").trigger("click");
-		$(".blog-page__head-filter._active").trigger("click");
-	}
-});
-
-$(function () {
     if ($('.contact-page').length) {
         let ind
         $('.contact-data-swiper .swiper-slide').each(function (i) {
@@ -285,6 +236,56 @@ $(function () {
     }
 })
 $(function () {
+	if ($(".blog-page").length) {
+		const BlogTypeSlider = new Swiper(".blog-type-slider", {
+			allowTouchMove: "false",
+			loop: false,
+			autoHeight: true,
+		});
+		$(".blog-page__head-filter").click(function () {
+			$(".blog-page__head-filter").removeClass("_active");
+			$(this).addClass("_active");
+			BlogTypeSlider.slideTo($(this).data("slideindex"));
+		});
+		$(".blog-page__filter").click(function () {
+			let filter = $(this).data("filter");
+			$(".blog-page__filter").removeClass("_active");
+			$(this).addClass("_active");
+			$(".aticle-preview").removeClass("_hidden");
+			$(".aticle-month-group").removeClass("_hidden");
+			$(".news-preview").removeClass("_hidden");
+			if (filter != "All") {
+				$(".aticle-month-group").each(function () {
+					let vLenght = 0;
+					$(this)
+						.find(".aticle-preview")
+						.each(function () {
+							if ($(this).data("filter") == filter) {
+								vLenght++;
+							} else {
+								$(this).addClass("_hidden");
+							}
+						});
+					if (vLenght == 0) {
+						$(this).addClass("_hidden");
+					} else {
+						$(this).removeClass("_hidden");
+					}
+				});
+				$(".news-preview").each(function () {
+					if ($(this).data("filter") != filter) {
+						$(this).addClass("_hidden");
+					}
+				});
+			}
+		});
+		$(".blog-page__filter._active").trigger("click");
+		$(".blog-page__head-filter._active").trigger("click");
+	}
+});
+
+
+$(function () {
 	let pagination = $(".stories-list").data("pagination");
 	let visible = 0;
 	$(".story-preview").each(function () {
@@ -341,7 +342,6 @@ $(function () {
     })
 })
 
-
 $(function () {
     $('.select2').each(function () {
         $(this).select2({
@@ -360,3 +360,4 @@ window.addEventListener('resize', () => {
         width = window.innerWidth;
     }
 });
+
